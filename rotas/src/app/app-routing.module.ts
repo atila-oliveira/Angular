a1b34f606886ba385743d6fdaeb56ab1f8c98978
atila-oliveira <atilateioli@gmail.com>
2021-11-07 17:45:38 -0300

    commit

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+//define os endpoints das urls e o componente que será exibido no endpoint especificado
+const routes: Routes = [
+  {path: 'cursos', component: CursosComponent},
+  {path:'curso/:id', component: CursoDetalheComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
+  {path: '', component: HomeComponent}];
+
+//const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CursosService } from '../cursos/cursos.service';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent implements OnInit {
+
+  id!: number;
+  inscricao: Subscription = new Subscription;
+  curso: any;
+//ActivatedRoute pega detalhes da rota como o parâmetro
+  constructor(private activatedRoute: ActivatedRoute, private cursoService: CursosService, private router: Router) { 
+    //this.id = this.activatedRoute.snapshot.params['id'];
+    //console.log(this.activatedRoute)
+  }
+
+  ngOnInit(): void {
+    this.inscricao = this.activatedRoute.params.subscribe((params:any) =>{
+      this.id = params['id']
+
+      this.curso = this.cursoService.getCurso(this.id)
+
+      if(this.curso == null){
+        this.router.navigate(['/naoEncontrado'])
+      }
+
+    } )
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe()
+  }
+
+}
